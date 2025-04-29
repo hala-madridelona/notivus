b2af36f4f75b8bcecbc75a90164330a3256265ac
Hala Madrid <shas5179hank@gmail.com>
2025-04-29 18:12:57 +0530

    [BUGFIX] Fix zod schema

--- src/components/forms/create-group.tsx
+++ src/components/forms/create-group.tsx
@@ -15,7 +15,6 @@ import { useState } from 'react';
 const formSchema = z.object({
   name: z.string().min(1).max(50),
   description: z.string().min(0).max(200),
-  tagName: z.string().min(2).max(50),
 });
 
 export const CreateGroupForm = ({
